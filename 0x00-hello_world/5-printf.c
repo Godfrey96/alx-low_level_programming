54df0e273f2f10a1949d1573f0208faeb7508f8d
Godfrey96 <ngwatlegodfrey@gmail.com>
2022-06-30 17:21:45 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Return: 0 if successl
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is is a piece of art,\n");
+	return (0);
+}
