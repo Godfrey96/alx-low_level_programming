90a72e240d4f025e190b09989b98fe0fd0630920
Godfrey96 <ngwatlegodfrey@gmail.com>
2022-07-03 00:38:39 -0700

    print all alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * print all alphabet letters in lowercase except q and e
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char ch = 'a';
+	int count  = 0;
+
+	do {
+		if(ch != 'q' && ch != 'e')
+			putchar(ch);
+		ch++;
+		count++;
+	} while (count < 26);
+	putchar('\n');
+	return (0);
+}
