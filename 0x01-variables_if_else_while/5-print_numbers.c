4c23d9d0847181831aa4dfa8dff8e3898520a190
Godfrey96 <ngwatlegodfrey@gmail.com>
2022-07-03 00:45:50 -0700

    print base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * program to print base 10 numbers
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
