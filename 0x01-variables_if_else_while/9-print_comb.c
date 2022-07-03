ad83de222344998c220864d542e0a4da2bdc39f4
Godfrey96 <ngwatlegodfrey@gmail.com>
2022-07-03 01:28:02 -0700

    print combination of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * print all combination of single digit numbers seperated by commas and space'Return: 0 if successful
+ */
+int main(void)
+{
+	int i = 48;
+
+	do {
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	} while (i <= 57);
+	putchar('\n');
+	return (0);
+}
