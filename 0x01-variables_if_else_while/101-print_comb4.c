4ccd584ce0bb1f7dec18d30462353e9f76930c05
Godfrey96 <ngwatlegodfrey@gmail.com>
2022-07-03 02:37:32 -0700

    fixed the initialization

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,16 +7,17 @@
 int main(void)
 {
 	int i, j, k;
-	i = i % 10;
-	j = j % 10;
-	k = k % 10;
-
+	
 	for (i = 0; i < 8; i++)
 	{
 		for (j = i + 1; j < 9; j++)
 		{
 			for ( k = j + 1; k < 10; k++)
 			{
+				i = i % 10;
+				j = j % 10;
+				k = k % 10;
+
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
