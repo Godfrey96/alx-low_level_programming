f960edd839d67ee1cbe67feec05963c5e67c889b
Godfrey96 <ngwatlegodfrey@gmail.com>
2022-07-07 08:25:54 -0700

    a main program and a program that test for positive or negative integer

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - a program that test for positiv or negative
+ * Return: 0 on successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	positiv_or_negative(i);
+	return (0);
+}
