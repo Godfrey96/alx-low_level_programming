e77c5432fd6f640891487b633f03bc47edcf9db2
Godfrey96 <ngwatlegodfrey@gmail.com>
2022-07-08 06:56:10 -0700

    space added on each word and number

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,14 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz ");
+			printf(" Fizz");
 		else if (i % 5 == 0)
-			printf("Fizz ");
+			printf(" Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
-	printf("Buzz\n");
+	printf("\n");
 	return (0);
 }
