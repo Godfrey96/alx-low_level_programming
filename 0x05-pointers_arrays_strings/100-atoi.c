b8291a050b45b6a316aa615465b85d6b216f59f7
Godfrey96 <ngwatlegodfrey@gmail.com>
2022-07-12 18:50:51 -0700

    used the correct unary

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,12 +11,12 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int n = 0;
 
-	while (*sign++)
+	while (*s++)
 	{
-		if (*sign == '-')
+		if (*s == '-')
 			sign *= -1;
-		else if (*sign >= '0' && *sign <= '9')
-			n = (n * 10) + (*sign - '0');
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
 		else if (n > 0)
 			break;
 	}
