8a1e15295784d431bb5622ff6d8b3ed027ae97a4
Godfrey96 <ngwatlegodfrey@gmail.com>
2022-07-21 12:12:32 -0700

    fixed the sqrt function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,7 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - the natural square root of a number.
- * @n: the number
- * Return: 0 on successful
- */
-
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt(n, 0));
-}
-
-/**
- * sqroot - the sqrt function
+ * _sqrt - the sqrt function
  * @a: the number
  * @b: the number
  * Return: 0 on successful
@@ -29,4 +14,19 @@ int _sqrt(int a, int b)
 	if (b * b == a)
 		return (b);
 	return (_sqrt(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - the natural square root of a number.
+ * @m: the number
+ * Return: 0 on successful
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(n, 0));
 }
